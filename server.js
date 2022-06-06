8e962edc5f05a831aaa9860fe1802511d16a5875
BlessingAgbor <helenblessing14200@gmail.com>
2022-06-06 18:45:09 +0100

    afterClass

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require("./utils/db")
 const express = require("express")
-const port = 9000;
+require("dotenv").config()
+const port = process.env.PORT || 9000;
 const cors = require("cors")
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.get("/", (req, res)=>{
-    res.send("Welcome to Social App")
+    res.send("Welcome to Social App Backend")
 })
 app.listen(port, ()=>{
     console.log("Listening to port: ", port)
 })
+
 
